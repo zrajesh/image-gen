02b11c69e57865f55b90bb8ba398e94b7661cce7
zrajesh <rajesh.bhattarai@payworldindia.com>
2023-03-16 22:00:10 +0530

    setting up deployed api

--- image-gen-client/src/pages/CreatePost.tsx
+++ image-gen-client/src/pages/CreatePost.tsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
         if (form.prompt && form.photo && form.name) {
             setLoading(true);
             try {
-                const response = await fetch("http://localhost:8080/api/v1/post", {
+                const response = await fetch("https://image-gen-42fz.onrender.com/api/v1/post", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
@@ -52,7 +52,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch("http://localhost:8080/api/v1/dalle", {
+                const response = await fetch("https://image-gen-42fz.onrender.com/api/v1/dalle", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
